e6129c9f30aba65bda27d0bcf022e0c0248c60c7
JoaoAlmir <junivion@gmail.com>
2022-08-23 19:46:16 -0300

    comentarios adicionados

--- config/express.js
+++ config/express.js
@@ -1,9 +1,11 @@
+
 const express = require("express");
 const routes_aluno = require("../app/route/alunos.route");
 
 module.exports = function(){
     let app = express();
-    app.set("port", 3000);
+    //Definindo variavel de aplicação
+    app.set("port", 8393);
     routes_aluno(app);
     app.use(express.static("./public"))
     return app;
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const http = require("http");
 const express_app = require("./config/express");
 
+//chama a função de express_app
 let app = express_app();
+
 http.createServer(app).listen(app.get("port"), function(){
     console.log("Servidor express está rodando na porta: " + app.get("port"));
 });
