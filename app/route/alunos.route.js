1d62eaaea294f19f3e78399923798956ae79a344
joaoAlmir <junivion@gmail.com>
2022-10-05 14:39:22 -0300

    autenticação incompleta

--- app/route/alunos.route.js
+++ app/route/alunos.route.js
@@ -1,9 +1,14 @@
 const controller = require("../controller/alunos.controller");
-
+const controller_auth = require("../controller/auth")
 module.exports = function(app){
+
+
     app.post("/alunos/login", controller.login);
+    app.delete("/alunos/:id",controller.removerAluno);
+    app.post("/alunos", controller.inserirAluno);
+    
+    app.use("/alunos",controller_auth.checar_token);
+
     app.get("/alunos", controller.obterAlunos);
     app.get("/alunos/:id", controller.obterAluno);
-    app.post("/alunos", controller.inserirAluno);
-    app.delete("/alunos/:id",controller.removerAluno);
 }
